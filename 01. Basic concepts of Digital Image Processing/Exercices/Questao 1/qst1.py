6f9b4337d8e4a268232a6da6fd07f9d16daca2b7
David Alexandre <38302314+DavidAlexandreTS@users.noreply.github.com>
2020-03-19 23:15:20 -0300

    Finalizando

--- 01. Basic concepts of Digital Image Processing/Exercices/Questao 1/qst1.py
+++ 01. Basic concepts of Digital Image Processing/Exercices/Questao 1/qst1.py
@@ -14,7 +14,7 @@ imgColorida = cv2.imread('moedas.jpg') #Carregamento da imagem
 
 #Conversão para tons de cinza
 img = cv2.cvtColor(imgColorida, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("Cinza.jpg", img)
+cv2.imwrite("01. Imagem Cinza.jpg", img)
 
 #Blur/Suavização da imagem
 suave = cv2.blur(img, (7, 7))
@@ -25,7 +25,7 @@ bin = suave.copy()
 bin[bin > T] = 255
 bin[bin < 255] = 0
 bin = cv2.bitwise_not(bin)
-cv2.imwrite("Imagem Binarizada.jpg", img)
+cv2.imwrite("02. Imagem Binarizada.jpg", bin)
 
 #Detecção de bordas com Canny
 bordas = cv2.Canny(bin, 70, 150)
@@ -39,18 +39,28 @@ temp = np.vstack([
     np.hstack([bin, bordas])
     ])
 
+#Desenho dos contornos
 imgC2 = imgColorida.copy()
-cv2.drawContours(imgC2, objetos, -1, (255, 0, 0), 2)
+final = cv2.drawContours(imgC2, objetos, -1, (255, 0, 0), 2)
 escreve(imgC2, str(len(objetos))+" moedas foram encontradas!")
-cv2.imwrite("Resultado.jpg", imgC2)
-#cv2.imwrite("Cinza.png", bin)
+cv2.imwrite("03. Imagem com as Bordas Destacadas.jpg", imgC2)
 
-n, bins, patches = plt.hist(bin.ravel(), 256, [1, 255])
-plt.show()
+n_moedas = 0;
+for i in objetos:
+    n_moedas += 1
+    M = cv2.moments(i)
 
-#for y in range(0, len(objetos)):
-#    n, bins, patches = plt.hist(objetos[y].ravel(), 256, [1, 255])
-#    plt.show()
-    #print(y)
+    #Cálculo da Área
+    area = cv2.contourArea(i)
+
+    #Cálculo da posição a partir do Centroide
+    x = int(M['m10']/M['m00'])
+    y = int(M['m01']/M['m00'])
+    posicao = (x - 150, y)
+
+    #Cálculo do Perímetro
+    perimetro = cv2.arcLength(i, True)
+
+    print("Objeto {}:\nArea: {}\nCentro: ({},{})\nPerimetro: {}\n".format(n_moedas, area, x, y, perimetro))
 
 cv2.waitKey(0)
